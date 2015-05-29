f7fa4f4e22a5537ee218d04f8a566b5f5ebd870f
sebbrochet <contact@sebbrochet.com>
2015-05-29 20:51:52 +0200

    Added basic tutorial (Album) with sqlite instead of MySQL

--- application/config/autoload/global.php
+++ application/config/autoload/global.php
@@ -11,6 +11,11 @@
  * file.
  */
 
-return array(
-    // ...
+return array(    
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
---
+++ application/module/Album/config/module.config.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/album[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+    
+    'db' => array( //module can have a specific db configuration
+        'driver' => 'PDO_SQLite',
+        'dsn' => 'sqlite:' . __DIR__ .'/../data/album.db',
+        'driver_options' => array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        )
+    )
+);
+
